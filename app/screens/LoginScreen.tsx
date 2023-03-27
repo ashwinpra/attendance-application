096b7533d5aa9a60bad24a933c6380ed1e00aef5
Sanskar <sanskarmittal2002@gmail.com>
2023-03-27 20:52:32 +0530

    Added login screen

--- app/screens/LoginScreen.tsx
+++ app/screens/LoginScreen.tsx
@@ -1,5 +1,11 @@
-import React from "react";
-import { View, Text } from "react-native";
+import React, { useState } from "react";
+import {
+  StyleSheet,
+  View,
+  Text,
+  TextInput,
+  TouchableOpacity,
+} from "react-native";
 import { NativeStackNavigationProp } from "@react-navigation/native-stack";
 
 type RootStackParamList = {
@@ -11,10 +17,82 @@ type Props = {
   navigation: NativeStackNavigationProp<RootStackParamList, "Home">;
 };
 
+/*
+//make a full fledged login screen
 export default function LoginScreen({ navigation }: Props) {
   return (
     <View>
-      <Text>Welcome Back!</Text>
+      <Text>Login Screen</Text>
+
     </View>
   );
 }
+*/
+
+const LoginScreen: React.FC<Props> = ({ navigation }) => {
+  const [username, setUsername] = useState<string>("");
+  const [password, setPassword] = useState<string>("");
+
+  const handleLogin = () => {
+    // handle login logic
+    navigation.navigate("Home");
+  };
+
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>Login</Text>
+      <TextInput
+        style={styles.input}
+        placeholder="Username"
+        onChangeText={(text) => setUsername(text)}
+        value={username}
+      />
+      <TextInput
+        style={styles.input}
+        placeholder="Password"
+        onChangeText={(text) => setPassword(text)}
+        value={password}
+        secureTextEntry={true}
+      />
+      <TouchableOpacity style={styles.button} onPress={handleLogin}>
+        <Text style={styles.buttonText}>Login</Text>
+      </TouchableOpacity>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+    backgroundColor: "#fff",
+  },
+  title: {
+    fontSize: 24,
+    fontWeight: "bold",
+    marginBottom: 20,
+  },
+  input: {
+    width: "80%",
+    height: 40,
+    borderWidth: 1,
+    borderColor: "#ccc",
+    borderRadius: 5,
+    padding: 10,
+    marginBottom: 20,
+  },
+  button: {
+    backgroundColor: "#008CBA",
+    borderRadius: 5,
+    paddingVertical: 10,
+    paddingHorizontal: 20,
+  },
+  buttonText: {
+    color: "#fff",
+    fontSize: 18,
+    fontWeight: "bold",
+  },
+});
+
+export default LoginScreen;
